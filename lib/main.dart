d09ffda30b5af36b34e4882a25a7b62c76de9998
rothanang21 <rothanang21@gmail.com>
2024-08-11 08:12:50 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Simple Flutter App'),
+        ),
+        body: Container(
+          constraints: BoxConstraints(maxHeight: 600, maxWidth: 400),
+          // margin: EdgeInsets.only(left: 80, top: 50),
+          decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              // borderRadius: BorderRadius.circular(10),
+              color: Color(0xff4361ee),
+              boxShadow: [
+                BoxShadow(color: Colors.green, blurRadius: 6, spreadRadius: 4),
+              ]),
+          // width: 200,
+          // height: 200,
+          alignment: Alignment.center,
+          transform: Matrix4.rotationZ(0.0),
+          padding: EdgeInsets.all(10),
+          // color: Colors.pink,
+          child: Text(
+            'Flutter',
+            style: TextStyle(
+                color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
